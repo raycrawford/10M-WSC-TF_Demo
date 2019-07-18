a571c63cba4b14ab462ae7ef5166f30031e0fb45
Ray Crawford <ray.crawford@gmail.com>
2019-07-18 08:45:24 -0400

    Updating for a changed IP

--- nsg-dmz.tf
+++ nsg-dmz.tf
@@ -40,7 +40,7 @@ resource "azurerm_network_security_rule" "dmz_allow_external_inbound" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefixes     = ["72.49.23.246", "12.154.222.62"]
+  source_address_prefixes     = ["72.49.23.54", "12.154.222.62"]
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.WSC.name
   network_security_group_name = azurerm_network_security_group.dmz.name
